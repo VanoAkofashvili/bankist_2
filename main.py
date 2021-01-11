2e8234a62ced41b9ad0d94f5c222463ac85301b0
VanoAkofashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-01-12 00:57:02 +0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    def __init__(self):
-        attrs = ["name", "age", "mother", "father"]
-        for attr in attrs:
-            self.attr = attr
-
-
-obj = Test()
-print(obj.__dict__)
